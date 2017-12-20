db4c0497cdc64868d4db3f8a7ddb8ce5ae08db7f
viaeoo <viaeoo@gmail.com>
2017-12-21 00:37:46 +0900

    remove swagger

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render(swaggerUi.serve, swaggerUi.setup(null, null, null, null, null));
+  res.render();
 });
 
 module.exports = router;
