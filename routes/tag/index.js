e8c67b2f7dd482887c364fa53124e60fe4e4b560
viaeoo <viaeoo@gmail.com>
2018-01-01 01:47:14 +0900

    tag api modify

--- routes/tag/index.js
+++ routes/tag/index.js
@@ -1,127 +1,23 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const error = require(path.join(__dirname, '../../helper/errorHandler'));
 
 const router = express.Router();
-const Tag = require(path.join(__dirname, './tag.model.js'));
+const Controller = require(path.join(__dirname, './tag.controller.js'));
 
 // get all tag list
-router.get('/', function(req, res, next) {
-  console.log("get all tag list");
-
-  Tag.findAll({
-    where: {
-      // todo
-    },
-  })
-  .then(results => {
-    res.status(200).json(results);
-  })
-  .catch(err => {
-    next(err);
-  });
-});
+router.get('/', Controller.getAllTag);
 
 // get tag
-router.get('/:tag_id', function(req, res, next) {
-  console.log("get a specific tag");
-
-  Tag.findOne({
-    where: {
-      tag_id: req.params.tag_id,
-    },
-  })
-  .then(result => {
-    result ? res.status(200).json(result) : next(error(400));
-  })
-  .catch(err => {
-    next(err);
-  });
-});
+router.get('/:tag_id', Controller.getTag);
 
 // create tag
-router.post('/', function(req, res, next) {
-  console.log("Create a tag");
-
-  Tag.findOrCreate({
-    where: {
-      tag_id: req.body.tag_id,
-      tag_name: req.body.tag_name
-      //...
-    }
-  })
-  .spread((tag, created) => {
-    created ? res.status(201).json(tag) : next(error(400));
-  })
-  .catch(err => {
-    next(err);
-  });
-});
+router.post('/', Controller.createTag);
 
 // delete tag
-router.delete('/:tag_id', function(req, res, next) {
-  console.log("Remove a tag");
-  
-  Tag.findById(req.params.tag_id)
-  .then(num => {
-    // number (0 or 1)
-    if(num) {
-      Tag.destroy({
-        where: {
-          tag_id: req.params.tag_id,
-        }
-      })
-      .then(result => {
-        res.send(200);
-      })
-      .catch(err => {
-        next(err);
-      });
-    }
-    else {
-      next(error(400));
-    }
-  })
-  .catch(err => {
-    next(err);
-  });
-});
+router.delete('/:tag_id', Controller.deleteTag);
 
 // update tag
-router.put('/:tag_id', function(req, res, next) {
-  // update list
-  let updateList = {
-    tag_name: req.body.tag_name
-    //...
-  };
-
-  Tag.findById(req.params.tag_id)
-  .then(num => {
-    // number (0 or 1)
-    if(num) {
-      Tag.update(updateList, {
-        where: {
-          tag_id: req.params.tag_id,
-        }
-      })
-      .then(result => {
-        // result is number (o or 1)
-        // 0: 기존 데이터와 동일
-        // 1: 기존 데이터와 달라 업데이트 성공
-        res.status(201).send(result);
-      })
-      .catch(err => {
-        next(err);
-      }); 
-    }
-    else {
-      next(error(400));
-    }
-  })
-  .catch(err => {
-    next(err);
-  });
-});
+router.put('/:tag_id', Controller.updateTag);
 
 module.exports = router;
---
+++ routes/tag/tag.controller.js
@@ -0,0 +1,136 @@
+const path = require('path');
+const error = require(path.join(__dirname, '../../helper/errorHandler'));
+
+const Tag = require(path.join(__dirname, './tag.model.js'));
+
+exports.getAllTag = function(req, res, next) {
+  console.log("get all tag list");
+
+  const respond = results => {
+    res.status(200).json(results);
+  };
+
+  const onError = err => {
+    next(err);
+  };
+  
+  Tag.findAll()
+  .then(respond)
+  .catch(onError);
+};
+
+exports.getTag = function(req, res, next) {
+  console.log("get a specific tag");
+
+  const respond = result => {
+    result ? res.status(200).json(result) : next(error(400));
+  };
+
+  const onError = err => {
+    next(err);
+  };
+
+  Tag.findOne({
+    where: {
+      tag_id: req.params.tag_id,
+    },
+  })
+  .then(respond)
+  .catch(onError);
+};
+
+exports.createTag = function(req, res, next) {
+  console.log("Create a tag");
+
+  const createList = {
+    tag_id,
+    tag_name
+    //...
+  } = req.body;
+
+  const respond = (tag, created) => {
+    created ? res.status(201).json(tag) : next(error(400));
+  };
+
+  const onError = err => {
+    next(err);
+  };
+
+  Tag.findOrCreate({
+    where: createList
+  })
+  .spread(respond)
+  .catch(onError);
+};
+
+exports.deleteTag = function(req, res, next) {
+  console.log("Remove a tag");
+
+  const respond = num => {
+    // number (0 or 1)
+    if(num) {
+      Tag.destroy({
+        where: {
+          tag_id: req.params.tag_id,
+        }
+      })
+      .then(result => {
+        res.send(200);
+      })
+      .catch(err => {
+        next(err);
+      });
+    }
+    else {
+      next(error(400));
+    }
+  };
+
+  const onError = err => {
+    next(err);
+  };
+
+  Tag.findById(req.params.tag_id)
+  .then(respond)
+  .catch(onError);
+};
+
+exports.updateTag = function(req, res, next) {
+  console.log("Update a tag");
+
+  const updateList = {
+    tag_name,
+    //...
+  } = req.body;
+
+  const respond = num => {
+    // number (0 or 1)
+    if(num) {
+      Tag.update(updateList, {
+        where: {
+          tag_id: req.params.tag_id,
+        }
+      })
+      .then(result => {
+        // result is number (o or 1)
+        // 0: 기존 데이터와 동일
+        // 1: 기존 데이터와 달라 업데이트 성공
+        res.status(201).send(result);
+      })
+      .catch(err => {
+        next(err);
+      }); 
+    }
+    else {
+      next(error(400));
+    }
+  };
+
+  const onError = err => {
+    next(err);
+  };
+
+  Tag.findById(req.params.tag_id)
+  .then(respond)
+  .catch(onError);
+};
