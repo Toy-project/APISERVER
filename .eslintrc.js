0ede6702c0d3dc4e905fde0cd5f96d265c508b66
viaeoo <viaeoo@gmail.com>
2018-01-04 23:22:54 +0900

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    "extends": "airbnb",
-    "root": true,
-    "rules": {
-        "func-names": ["error", "never"],
-        "max-len": [2, 150, 4, {"ignoreUrls": true}],
-    }
+  "extends": "airbnb",
+  "root": true,
+  "env": {
+    "node": true,
+    "es6": true,
+  },
+  "rules": {
+    "indent": ["error", 2, { "VariableDeclarator": { "var": 2, "let": 2, "const": 3 }, "MemberExpression": 0 }],
+    "quotes": ["error", "single"],
+    "no-console": "warn",
+    "func-names": ["error", "never"],
+    "max-len": [2, 150, 4, {"ignoreUrls": true}],
+    "no-unused-vars": ["warn", { "args": "none" }],
+    "no-unused-expressions": ["error", { "allowTernary": true }],
+  }
 };
