eefa17f547e1117f9c2c274ae90069fa38319fcf
INHOLEE <sayyou0918@gmail.com>
2018-02-20 13:09:32 +0900

    updated nexmo

--- routes/nexmo/nexmo.controller.js
+++ routes/nexmo/nexmo.controller.js
@@ -10,27 +10,29 @@ const nexmo = new Nexmo({
 
 
 exports.postVerify = (req, res, next) => {
-  const data = {
-    number : req.body.number,
-    brand : nexmoHelper.brand,
-  }
-  Nexmo.verify.request(data, (err, result) => {
-    if(err) { next(err); }
-    else {
-      res.status(201).json(result);
-    }
-  });
+  res.send('hello');
+  // const data = {
+  //   number : req.body.number,
+  //   brand : nexmoHelper.brand,
+  // }
+  // Nexmo.verify.request(data, (err, result) => {
+  //   if(err) { next(err); }
+  //   else {
+  //     res.status(201).json(result);
+  //   }
+  // });
 };
 
 exports.postCheck = (req, res, next) => {
-  const data = {
-    request_id : req.body.request_id,
-    code : req.body.code,
-  }
-  Nexmo.verify.check(data, (err, result) => {
-    if(err) { next(err); }
-    else {
-      res.status(201).json(result);
-    }
-  });
+  res.send('hello');
+  // const data = {
+  //   request_id : req.body.request_id,
+  //   code : req.body.code,
+  // }
+  // Nexmo.verify.check(data, (err, result) => {
+  //   if(err) { next(err); }
+  //   else {
+  //     res.status(201).json(result);
+  //   }
+  // });
 };
