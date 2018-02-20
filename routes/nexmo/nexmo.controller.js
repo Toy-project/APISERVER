6db9715137dca1737e2fa50277f5876689699bef
INHOLEE <sayyou0918@gmail.com>
2018-02-20 12:28:02 +0900

    Updating nexmo

--- routes/nexmo/nexmo.controller.js
+++ routes/nexmo/nexmo.controller.js
@@ -1,29 +1,29 @@
 const error = require('../../helper/errorHandler');
 const * as nexmoHelper = require('../../helper/nexmoHelper');
-const Nexmo = require('../../config/nexmo');
-
-exports.postVerify = (req, res, next) => {
-  const data = {
-    number : req.body.number,
-    brand : nexmoHelper.brand,
-  }
-  Nexmo.verify.request(data, (err, result) => {
-    if(err) { next(err); }
-    else {
-      res.status(201).json(result);
-    }
-  });
-};
-
-exports.postCheck = (req, res, next) => {
-  const data = {
-    request_id : req.body.request_id,
-    code : req.body.code,
-  }
-  Nexmo.verify.check(data, (err, result) => {
-    if(err) { next(err); }
-    else {
-      res.status(201).json(result);
-    }
-  });
-};
+// const Nexmo = require('../../config/nexmo');
+//
+// exports.postVerify = (req, res, next) => {
+//   const data = {
+//     number : req.body.number,
+//     brand : nexmoHelper.brand,
+//   }
+//   Nexmo.verify.request(data, (err, result) => {
+//     if(err) { next(err); }
+//     else {
+//       res.status(201).json(result);
+//     }
+//   });
+// };
+//
+// exports.postCheck = (req, res, next) => {
+//   const data = {
+//     request_id : req.body.request_id,
+//     code : req.body.code,
+//   }
+//   Nexmo.verify.check(data, (err, result) => {
+//     if(err) { next(err); }
+//     else {
+//       res.status(201).json(result);
+//     }
+//   });
+// };
