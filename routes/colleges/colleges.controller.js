0e83d74da64bba1c225c8bda8c9127a1e8efc407
INHOLEE <sayyou0918@gmail.com>
2018-03-06 20:50:13 +0900

    edited

--- routes/colleges/colleges.controller.js
+++ routes/colleges/colleges.controller.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 
 exports.getColleges = function (req, res, next) {
   const url = 'www.career.go.kr/cnet/openapi/getOpenApi?apiKey=3942376907ca20d1edc9bdb7f31c5696&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list';
-  // const start = req.params.start || req.query.start;
-  // const end = req.params.end || req.query.end;
+  const start = req.params.start || req.query.start;
+  const end = req.params.end || req.query.end;
 
   const onError = (err) => {
     next(err);
@@ -14,12 +14,12 @@ exports.getColleges = function (req, res, next) {
     results ? res.status(200).json(results) : next(error(400));
   };
 
-  // axios({
-  //   method: 'get',
-  //   timeout: 20000,
-  //   url: `${url}&thisPage=${start}&perPage=${end}`,
-  //   responseType: 'json'
-  // })
-  // .then(respond)
-  // .catch(onError);
+  axios({
+    method: 'get',
+    timeout: 20000,
+    url: `${url}&thisPage=${start}&perPage=${end}`,
+    responseType: 'json'
+  })
+  .then(respond)
+  .catch(onError);
 };
