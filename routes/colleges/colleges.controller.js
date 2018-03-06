f59472b64c5f6a68fbb1717fbb18dd67ddb56966
INHOLEE <sayyou0918@gmail.com>
2018-03-06 20:47:35 +0900

    edited

--- routes/colleges/colleges.controller.js
+++ routes/colleges/colleges.controller.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 
 exports.getColleges = function (req, res, next) {
   const url = 'www.career.go.kr/cnet/openapi/getOpenApi?apiKey=3942376907ca20d1edc9bdb7f31c5696&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list';
-  const start = req.params.start || req.query.start;
-  const end = req.params.end || req.query.end;
+  // const start = req.params.start || req.query.start;
+  // const end = req.params.end || req.query.end;
 
   const onError = (err) => {
     next(err);
