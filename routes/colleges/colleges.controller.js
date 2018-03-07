466ced0584a1de88b6687ea42818e4ea7b41e7e1
INHOLEE <sayyou0918@gmail.com>
2018-03-07 13:59:25 +0900

    editted colleges api

--- routes/colleges/colleges.controller.js
+++ routes/colleges/colleges.controller.js
@@ -2,9 +2,11 @@ const error = require('../../helper/errorHandler');
 const axios = require('axios');
 
 exports.getColleges = function (req, res, next) {
-  const url = 'http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=3942376907ca20d1edc9bdb7f31c5696&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list';
   const start = req.params.start || req.query.start;
   const end = req.params.end || req.query.end;
+  const keyword = req.params.keyword || req.query.keyword;
+
+  const url = `http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=3942376907ca20d1edc9bdb7f31c5696&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${start}&perPage=${end}&searchSchulNm=${keyword}`;
 
   const onError = (err) => {
     next(err);
@@ -17,7 +19,7 @@ exports.getColleges = function (req, res, next) {
   axios({
     method: 'get',
     timeout: 20000,
-    url: `${url}&thisPage=${start}&perPage=${end}`,
+    url: url,
   })
   .then(respond)
   .catch(onError);
