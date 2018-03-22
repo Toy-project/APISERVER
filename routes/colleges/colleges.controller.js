9a33868501691467bb56d286ef6410b0bb1897b9
INHOLEE <sayyou0918@gmail.com>
2018-03-22 18:57:23 +0900

    fixed college change

--- routes/colleges/colleges.controller.js
+++ routes/colleges/colleges.controller.js
@@ -7,6 +7,7 @@ exports.getColleges = function (req, res, next) {
   const keyword = req.params.keyword || req.query.keyword;
 
   const url = `http://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=3942376907ca20d1edc9bdb7f31c5696&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${start}&perPage=${end}&searchSchulNm=${encodeURI(keyword)}`;
+
   const onError = (err) => {
     next(err);
   };
